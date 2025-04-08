26bf1056b3fa78fc2944b48b5a10355b0d6683ea
gokifujiya <goki.fujiya@gmail.com>
2025-04-08 14:00:55 +0900

    Add RPC functions module

---
+++ rpc_functions.py
@@ -0,0 +1,7 @@
+import math
+
+def floor(x): return math.floor(x)
+def nroot(n, x): return round(x ** (1 / n), 5)
+def reverse(s): return s[::-1]
+def validAnagram(str1, str2): return sorted(str1) == sorted(str2)
+def sort(strArr): return sorted(strArr)
